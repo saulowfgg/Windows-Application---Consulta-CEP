9090333e2fd34309625a7205a77eb0bd02ef1018
Saulo Florencio <saulo.wfgg@gmail.com>
2019-09-20 10:27:39 -0400

    Windows Application - Localizar CEP

---
+++ LocalizarCep/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace LocalizarCep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.picLocalizar = new System.Windows.Forms.PictureBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picLocalizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CEP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Endereço";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Bairro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Cidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "UF";
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(258, 196);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(100, 20);
+            this.txtUF.TabIndex = 5;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(32, 196);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(100, 20);
+            this.txtCidade.TabIndex = 6;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(32, 42);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 7;
+            this.txtCEP.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCEP_KeyDown);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(32, 84);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(326, 20);
+            this.txtEndereco.TabIndex = 8;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(32, 136);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(326, 20);
+            this.txtBairro.TabIndex = 9;
+            // 
+            // picLocalizar
+            // 
+            this.picLocalizar.Image = ((System.Drawing.Image)(resources.GetObject("picLocalizar.Image")));
+            this.picLocalizar.Location = new System.Drawing.Point(138, 42);
+            this.picLocalizar.Name = "picLocalizar";
+            this.picLocalizar.Size = new System.Drawing.Size(22, 20);
+            this.picLocalizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLocalizar.TabIndex = 10;
+            this.picLocalizar.TabStop = false;
+            this.picLocalizar.Click += new System.EventHandler(this.picLocalizar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSair.Location = new System.Drawing.Point(85, 227);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(201, 227);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 262);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.picLocalizar);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Consultar CEP";
+            ((System.ComponentModel.ISupportInitialize)(this.picLocalizar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.PictureBox picLocalizar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ LocalizarCep/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace LocalizarCep
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void picLocalizar_Click(object sender, EventArgs e)
+        {
+            
+             try
+            {
+           
+                DataSet ds = new DataSet();
+                String xml = "https://viacep.com.br/ws/@cep/xml/".Replace("@cep", txtCEP.Text);
+                ds.ReadXml(xml);
+                txtEndereco.Text = ds.Tables[0].Rows[0]["logradouro"].ToString();
+                txtBairro.Text = ds.Tables[0].Rows[0]["bairro"].ToString();
+                txtCidade.Text = ds.Tables[0].Rows[0]["localidade"].ToString();
+                txtUF.Text = ds.Tables[0].Rows[0]["uf"].ToString();
+                
+                
+                if (File.Exists("Historico_Ceps.csv"))
+                {
+                    using (StreamWriter escritor = new StreamWriter("Historico_Ceps.csv", true)) 
+                        escritor.WriteLine( txtCEP.Text + ";" + @"""" + txtEndereco.Text + @"""" + ";" + txtBairro.Text + ";" + txtCidade.Text + ";" + txtUF.Text);
+                    
+                }
+                else
+                {
+                    StreamWriter escritor = new StreamWriter("Historico_Ceps.csv");
+                    escritor.WriteLine("CEP;Endereço;Bairro;Cidade;UF");
+                    escritor.WriteLine(txtCEP.Text + ";" + @"""" + txtEndereco.Text + @"""" + ";" + txtBairro.Text + ";" + txtCidade.Text + ";" + txtUF.Text);
+                    escritor.Close();
+
+                }
+                
+                
+            }
+            catch
+            {
+                txtCEP.Focus();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCEP.Text = "";
+            txtEndereco.Text = "";
+            txtBairro.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+            txtCEP.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void txtCEP_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                picLocalizar_Click(sender, e);
+        }
+    }
+}
